5c3bf74f90d02ea37e19fde26503b5a140a4278f
rui sp <manuelsapereira@gmail.com>
2023-10-20 12:12:40 +0800

    Added the plots component to the App component and  the api call.
     The App componenet calls the get_topic_sentiment endpoint of the topic
     search api and loads it at begining. The model selector allows the user
     to pick a sentiment time-plot for each of the SEC-10K models.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import ModelSelector from './ModelSelector';
+import Plots from './Plots';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const URL_API = 'http://127.0.0.1:8037/get_topics_sentiment';
 
 function App() {
-  return (
+  const [model, setModel] = useState('Section1'); // Default model
+  const [fetchedData, setFetchedData] = useState(null); // Data fetched from server
+
+  useEffect(() => {
+    const fetchDataOnce = async () => {
+      const response = await axios.get(URL_API, {params: { freq: '1y' }});
+      setFetchedData(response.data); // Now we have all the data for all the models
+    };
+
+    fetchDataOnce();
+  }, []); 
+  
+  return fetchedData ? (
     <><div className="App-title">
       <h1>SEC Filings: Topics and sentiment</h1>
     </div>
     <div className="App">
-    <ModelSelector/>
+    <ModelSelector model={model} setModel={setModel}/>
       <header className="App-header">
+        <Plots dataForPlot={fetchedData[model]} model={model}/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -23,8 +41,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div></>
-  );
+    </div></> ) : <div>Loading...</div> 
 }
 
 export default App;
