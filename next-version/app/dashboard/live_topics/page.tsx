24238b50281f0200dd96c1adcb9ead6fae0b8652
rui sp <manuelsapereira@gmail.com>
2023-11-24 16:06:04 +0800

    Adds pages and Sidenav component to dashboard
    Implemented three new page files 'topics_over_time/page.tsx' and 'live_topics/page.tsx' with placeholders inside the dashboard directory. Also created a 'sidenav.tsx' - a side navigation component for the dashboard.

---
+++ next-version/app/dashboard/live_topics/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Topis on URL',
+  };
+
+export default function Page() {
+    return <p>Placeholder for live_topics using url search</p>;
+}
---
+++ next-version/app/dashboard/topics_over_time/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Topis on URL',
+  };
+
+export default function Page() {
+    return <p>Placeholder for topics over time</p>;
+}
