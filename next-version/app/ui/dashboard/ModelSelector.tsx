21443bb6a44f54f4abbd74b2686725db1fcad288
rui sp <manuelsapereira@gmail.com>
2023-12-06 16:30:46 +0800

    Improves ModelSelector functionality and UI.
    Enhanced ModelSelector component to generate options from modelDescriptions object rather than hard-coded values. I also adds a box inside a Modal component. This aims to enhance the user experience, providing more comprehensive information about each model within the dashboard.

--- next-version/app/ui/dashboard/ModelSelector.tsx
+++ next-version/app/ui/dashboard/ModelSelector.tsx
@@ -1,13 +1,35 @@
 import React, {useState} from "react";
 import { toast } from 'react-toastify';
+import Select from "react-select";  // Import OptionTypeBase
+import {Tooltip} from '@chakra-ui/react';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+
 import 'react-toastify/dist/ReactToastify.css';
 
-const modelDescriptions = {
+const modelDescriptions: {[key: string]: string} = {
   Section1: 'Business Description',
   Section1A: 'Main Risks',
   Section7: 'Management Discussions',
    // add as many models as you need
  };
+
+ const sectionsDescriptions: { [key: string]: string } = {
+  Section1: `Description of the company’s business, including its main 
+     products and services, what subsidiaries it owns, and what markets it operates 
+     in. This section may also include information about recent events, competition 
+     the company faces, regulations that apply to it, labor issues, special operating 
+     costs, or seasonal factors.`,
+  Section1A: `Includes information about the most significant risks that apply 
+      to the company or to its securities. Companies generally list the risk factors 
+      in order of their importance. In practice, this section focuses on the risks 
+      themselves, not how the company addresses those risks. Some risks may be true 
+      for the entire economy, some may apply only to the company’s industry sector 
+      or geographic region, and some may be unique to the company.`,
+  Section7: `Gives the company’s perspective on the business results of the past 
+      financial year. This section, known as the MD&A for short, allows company 
+      management to tell its story in its own words.`
+};
 
  interface ModelSelectorProp {
   model: string;
@@ -15,30 +37,56 @@ const modelDescriptions = {
 }
 
 export default function ModelSelector({ model, setModel }: ModelSelectorProp) {
-    const [description, setDescription] = useState('Description for Model 1');
-    
+    const [description, setDescription] = useState(sectionsDescriptions['Section1']);
+    const [open, setOpen] = useState(false);
 
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
-    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setModel(event.target.value);
+    const options  = Object.keys(modelDescriptions).map(key => ({ value: key, label: modelDescriptions[key] }));
 
-        const modelDescription = modelDescriptions[event.target.value as keyof typeof modelDescriptions];
+    const handleChange = (selectedOption : any) => {
+        setModel(selectedOption.value);
+
+        const modelDescription = sectionsDescriptions[selectedOption.value as keyof typeof sectionsDescriptions];
         setDescription(modelDescription ? modelDescription : '');
-        toast.success(`Selected model: ${event.target.value}`);
+        toast.success(`Selected model trained on ${modelDescriptions[selectedOption.value]}  (${selectedOption.value} of the 10K)`);
     };
+    //Build the box        
+    const modalBody = (
+      <Box sx={{
+        position: 'absolute', top: '50%', left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: '30vw', bgcolor: 'rgba(255,255,255,1)', border: 'rounded-xl 2px solid #000',
+        boxShadow: 24, p: 4, overflow: 'scroll', borderRadius: '24pt'
+      }}>
+        {description}
+      </Box>
+    );
+      
 
     return (
       <div>
           <label htmlFor="models" > Choose a model: </label>
-         <div className="rounded-xl">
-          <select name="models" className="rounded-xl"
-           id="models" value={model} onChange={handleChange}>
-            {Object.keys(modelDescriptions).map(key => (
-          <option value={key} key={key}>{key}</option>
-        ))}
-          </select>
-          </div>
-        <p>{description}</p>
+          <Select  
+            className="rounded-xl w-64"
+            name="models"
+            value={options.find(option => option.value === model)} onChange={handleChange}
+            options={options}
+            placeholder="Select a model"
+            />
+            <button type="button" onClick={handleOpen}>
+          Open Model Description
+        </button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+        >
+       {modalBody}
+        </Modal>
+          <Tooltip label={description} fontSize="md" placement="bottom" width='200px' whiteSpace='normal'>
+            <p className="text-left text-xl font-semibold text-emph text-gray-800">Hover for a description</p>
+          </Tooltip>
         </div>
       );
 } 
